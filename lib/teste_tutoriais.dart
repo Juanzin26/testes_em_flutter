f32f693fe5eb35f215b83a4bb4dba3922cabc8c7
Juanzin26 <juanzitcho07@gmail.com>
2026-05-10 21:39:03 +0100

    commit 3, teste de operadores de comparacao

--- lib/teste_tutoriais.dart
+++ lib/teste_tutoriais.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 
-  print("Olá, mundo!");
-  print(1+1);
-  print(2*3);
-  print(6/2);
-  print(10%3);
+ print(9==9); // imprime true
+ print(9!=9); // imprime false
+ print(9>5); // imprime true
+ print(9<5); // imprime false
+ print(9>=9); // imprime true
+ print(9<=9); // imprime true
 }
 
 class MyApp extends StatelessWidget {
@@ -26,6 +27,16 @@ Operadores Basicos
 2 * 3 = 6, multiplicacao
 6 / 2 = 3, divisao
 10 % 3 = 1, resto da divisao
+
+Operadores de comparacao
+1 == 1, igual a, true
+1 != 2, diferente de, true
+1 > 0, maior que, true
+1 < 2, menor que, true
+3 < 2, menor que, false
+1 >= 1, maior ou igual a, true
+1 <= 2, menor ou igual a, true
+
  */
 
   @override
