e4fe7c3a9b6b9feda6a67ca4422353dca621016e
Juanzin26 <juanzitcho07@gmail.com>
2026-05-10 23:14:47 +0100

    commit cap14, widget text

--- lib/teste_tutoriais.dart
+++ lib/teste_tutoriais.dart
@@ -26,6 +26,14 @@ class MyApp extends StatelessWidget {
             padding: EdgeInsets.all(25), // Margem interna de todos os lados
             // padding: EdgeInsets.only(top: 25, left: 25), Margem interna do topo e da esquerda
             // padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25), // Margem interna horizontal e vertical
+            child: Text(
+              'Juanzin',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
