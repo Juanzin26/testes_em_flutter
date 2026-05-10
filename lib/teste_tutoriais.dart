1abd87040ac71e8bb481aca596b7d18beb23fbef
Juanzin26 <juanzitcho07@gmail.com>
2026-05-11 00:00:10 +0100

    commit cap 18, listview

--- lib/teste_tutoriais.dart
+++ lib/teste_tutoriais.dart
@@ -14,46 +14,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column( 
-
-          // crossAxisAlignment: CrossAxisAlignment.start, Alinhamento dos elementos no eixo horizontal, com todos os elementos alinhados à esquerda
-          // crossAxisAlignment: CrossAxisAlignment.center, Alinhamento dos elementos no eixo horizontal, com todos os elementos alinhados ao centro
-          // crossAxisAlignment: CrossAxisAlignment.end, Alinhamento dos elementos no eixo horizontal, com todos os elementos alinhados à direita
-          // mainAxisAlignment: MainAxisAlignment.center, no centro da tela
-          // mainAxisAlignment: MainAxisAlignment.end, no final da tela
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly, espaçamento igual entre os elementos
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween, o primeiro elemento fica no início, o último no final e os outros ficam espaçados entre eles
+        body: ListView( // o ListView permite criar uma lista rolável de widgets filhos.
+        // scrollDirection: Axis.horizontal, // para rolar horizontalmente
+        // scrollDirection: Axis.vertical, // para rolar verticalmente (definido assim por padrão)
 
           children: [
 
             // Container 1 
-            Expanded(
-              child: Container(
-              // width: 200, largura
-              // height: 200, altura
-                color: Colors.green,
-              ),
+            Container(
+              height: 350,
+              // width: 350
+              color: Colors.green,
             ),
 
-
             // Container 2
-            Expanded(
-              flex: 2, // O Container 2 ocupará o dobro do espaço em relação aos outros Containers
-              child: Container(
-              // width: 200,
-              // height: 200,
-                color: Colors.red,
-              ),
+            Container(
+              height: 350,
+              // width: 350
+              color: Colors.red,
             ),
 
 
             // Container 3
-            Expanded(
-              child: Container(
-                // width: 200,
-                // height: 200,
+              Container(
+                height: 350,
+                // width: 350
                 color: Colors.yellow,
-              ),
             ),
 
 
