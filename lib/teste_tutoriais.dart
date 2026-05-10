50006cb6ecf4ddea1518e4540436e5990f7e0fcb
Juanzin26 <juanzitcho07@gmail.com>
2026-05-10 21:28:03 +0100

    commit cap 2, teste dos operadores matematicos

--- lib/teste_tutoriais.dart
+++ lib/teste_tutoriais.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+
+  print("Olá, mundo!");
+  print(1+1);
+  print(2*3);
+  print(6/2);
+  print(10%3);
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +18,15 @@ String nome = "Juanzin";
 int idade = 20;
 double pi = 3.14;
 bool ehIniciante = true; // true or false values
+
+/*
+Operadores Basicos
+1 + 1 - 2, adicao
+2 - 1 = 1, subtracao
+2 * 3 = 6, multiplicacao
+6 / 2 = 3, divisao
+10 % 3 = 1, resto da divisao
+ */
 
   @override
   Widget build(BuildContext context) {
